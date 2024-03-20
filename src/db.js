ccc52ed8575b3ee14f134ea2809a77ad5c50fee7
16maax <maxcarrillo1616@gmail.com>
2024-03-20 12:09:38 -0600

    todo listo

--- src/db.js
+++ src/db.js
@@ -1,9 +1,16 @@
 import { createPool } from "mysql2/promise"
+import {
+    DB_HOST,
+    DB_PORT,
+    DB_DATABASE,
+    DB_USER,
+    DB_PASSWORD
+} from './config.js'
 
 export const pool = createPool ({
-    host: '162.241.61.125',
-    user: 'podermuj_admin_monday',
-    password: 'N&4Uq12DcbIH',
-    port: 3306,
-    database: 'podermuj_db_monday'
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    database: DB_DATABASE
 })
