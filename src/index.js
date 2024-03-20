3b42ef0bd8106c38cc46d3ab58bb03f81ad9229b
16maax <maxcarrillo1616@gmail.com>
2024-03-20 13:18:35 -0600

    cambios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-import app from './app.js'
-import {PORT} from './config.js'
+import express from 'express'
+import cors from 'cors'
+import usuariosRoutes from './routes/usuarios.routes.js'
+import indexRoutes from './routes/index.routes.js'
+import cobrarRoutes from './routes/cobrar.routes.js'
+import pagarRoutes from './routes/pagar.routes.js'
+import actividadesRoutes from './routes/actividades.routes.js'
+import inversionesRoutes from './routes/inversion.routes.js'
+import loginRoutes from './routes/login.routes.js'
 
-app.listen(PORT)
-console.log('Servidor en el puerto', PORT)
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use(indexRoutes)
+app.use(usuariosRoutes)
+app.use(cobrarRoutes)
+app.use(pagarRoutes)
+app.use(actividadesRoutes)
+app.use(inversionesRoutes)
+app.use(loginRoutes)
+
+app.listen(3000)
+console.log('Servidor en el puerto 3000')
