6e904150e01420fb3f0f949f2df11731d42efeef
16maax <maxcarrillo1616@gmail.com>
2024-03-21 11:44:58 -0600

    quite cors

--- src/app.js
+++ src/app.js
@@ -1,31 +1,23 @@
-import express from 'express';
-import cors from 'cors';
-import usuariosRoutes from './routes/usuarios.routes.js';
-import indexRoutes from './routes/index.routes.js';
-import cobrarRoutes from './routes/cobrar.routes.js';
-import pagarRoutes from './routes/pagar.routes.js';
-import actividadesRoutes from './routes/actividades.routes.js';
-import inversionesRoutes from './routes/inversion.routes.js';
-import loginRoutes from './routes/login.routes.js';
+import express from 'express'
+import cors from 'cors'
+import usuariosRoutes from './routes/usuarios.routes.js'
+import indexRoutes from './routes/index.routes.js'
+import cobrarRoutes from './routes/cobrar.routes.js'
+import pagarRoutes from './routes/pagar.routes.js'
+import actividadesRoutes from './routes/actividades.routes.js'
+import inversionesRoutes from './routes/inversion.routes.js'
+import loginRoutes from './routes/login.routes.js'
 
-const app = express();
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-// Configuración de CORS para permitir solicitudes desde https://podermujer.com.mx/
-const corsOptions = {
-  origin: 'https://podermujer.com.mx',
-};
-
-// Aplicar middleware de CORS con opciones personalizadas
-app.use(cors(corsOptions));
-
-// Resto de la configuración del servidor...
-app.use(express.json());
-app.use(indexRoutes);
-app.use(usuariosRoutes);
-app.use(cobrarRoutes);
-app.use(pagarRoutes);
-app.use(actividadesRoutes);
-app.use(inversionesRoutes);
-app.use(loginRoutes);
+app.use(indexRoutes)
+app.use(usuariosRoutes)
+app.use(cobrarRoutes)
+app.use(pagarRoutes)
+app.use(actividadesRoutes)
+app.use(inversionesRoutes)
+app.use(loginRoutes)
 
 export default app;
